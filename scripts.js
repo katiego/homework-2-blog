e12b8c8b2986a289a3cc4b60b09279655a42367d
katiego <katiegaudin@gmail.com>
2015-07-05 17:12:59 -0700

    added readme file

--- scripts.js
+++ scripts.js
@@ -5,55 +5,41 @@ $(function() {
     this.text = text;
   }
 
-  // `ToDo.all` contains our seed data
-  // ToDo.all = [];
+
  Post.all = [];
  console.log('works')
 
-  // ToDo.prototype.save = function() {
-  //   // store our new todo
-  //   ToDo.all.push(this);
-  //   console.log(ToDo.all);
-  // };
 
   Post.prototype.save = function() {
-    // store our new todo
+// store our new post
     Post.all.push(this);
     console.log(Post.all);
   };
 
-  // ToDo.prototype.render = function() {
-  //   // append our new todo to the page
-  //   var $todo = $(toDoTemplate(this));
-  //   this.index = ToDo.all.indexOf(this);
-  //   $todo.attr('data-index', this.index);
-  //   $toDoList.append($todo);
-  // };
+// render adn save new posts to the page / array
 
 Post.prototype.render = function() {
-    // append our new todo to the page
     var $post = $(postTemplate(this));
     this.index = Post.all.indexOf(this);
     $post.attr('data-index', this.index);
     $postList.append($post);
   };
 
-  // form to create new todo
+  // form to create new post
   var $newPost = $('#new-post');
 
-  // element to hold our list of todos
+  // element to hold our list of posts
   var $postList = $('#post-list');
 
-  // todo template
+  // post template
   var postTemplate = _.template($('#post-template').html());
 
-  // append existing todos (from seed data) to `$toDoList`
-  // `_.each` is an "iterator" function provided by Underscore.js
+  // append existing posts
   _.each(Post.all, function (post, index) {
     post.render();
   });
 
-  // submit form to create new todo
+  // submit form to create new post
   $newPost.on('submit', function(event) {
     event.preventDefault();
 
@@ -62,10 +48,10 @@ Post.prototype.render = function() {
     var postText = $('#post-text').val();
     var post= new Post(postDate, postText);
 
-    // save toDo
+    // save post
     post.save();
 
-    // render toDo
+    // render post
     post.render();
 
     // reset the form
@@ -78,19 +64,19 @@ Post.prototype.render = function() {
     $(this).toggleClass('done');
   });
 
-  // remove todo from model and view
+  // remove post from model and view
   $postList.on("click", ".delete", function () {
     var $post = $(this).closest(".post");
     var index = $post.attr('data-index');
 
-    // remove todo from the `ToDo.all` array (model)
+    // remove post from the `Post.all` array (model)
     post.all.splice(index, 1);
     console.log(Post.all);
 
-    // remove todo from the DOM (view)
+    // remove post from the DOM (view)
     $post.remove();
 
-    // reset indexes in DOM to match `ToDo.all` array
+    // reset indexes in DOM to match `Post.all` array
     // $.each loops through DOM elements
     $('.post').each(function(index) {
       $(this).attr('data-index', index);
