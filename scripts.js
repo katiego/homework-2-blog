a88a247254b4e91ffbb19dc5faad5f64d2bf3c79
katiego <katiegaudin@gmail.com>
2015-07-05 17:02:30 -0700

    all new posts appear on page

--- scripts.js
+++ scripts.js
@@ -1,87 +1,98 @@
 $(function() {
 
-  // constructor functions
-  function ToDo(name, desc) {
-    this.name = name;
-    this.desc = desc;
+  function Post(date, text) {
+    this.date = date;
+    this.text = text;
   }
 
   // `ToDo.all` contains our seed data
-  ToDo.all = [
-    new ToDo('laundry', 'clean clothes'),
-    new ToDo('grocery shopping', 'buy food'),
-    new ToDo('nap time', 'remember to sleep!')
-  ];
+  // ToDo.all = [];
+ Post.all = [];
+ console.log('works')
 
-  ToDo.prototype.save = function() {
+  // ToDo.prototype.save = function() {
+  //   // store our new todo
+  //   ToDo.all.push(this);
+  //   console.log(ToDo.all);
+  // };
+
+  Post.prototype.save = function() {
     // store our new todo
-    ToDo.all.push(this);
-    console.log(ToDo.all);
+    Post.all.push(this);
+    console.log(Post.all);
   };
 
-  ToDo.prototype.render = function() {
+  // ToDo.prototype.render = function() {
+  //   // append our new todo to the page
+  //   var $todo = $(toDoTemplate(this));
+  //   this.index = ToDo.all.indexOf(this);
+  //   $todo.attr('data-index', this.index);
+  //   $toDoList.append($todo);
+  // };
+
+Post.prototype.render = function() {
     // append our new todo to the page
-    var $todo = $(toDoTemplate(this));
-    this.index = ToDo.all.indexOf(this);
-    $todo.attr('data-index', this.index);
-    $toDoList.append($todo);
+    var $post = $(postTemplate(this));
+    this.index = Post.all.indexOf(this);
+    $post.attr('data-index', this.index);
+    $postList.append($post);
   };
 
   // form to create new todo
-  var $newToDo = $('#new-todo');
+  var $newPost = $('#new-post');
 
   // element to hold our list of todos
-  var $toDoList = $('#todo-list');
+  var $postList = $('#post-list');
 
   // todo template
-  var toDoTemplate = _.template($('#todo-template').html());
+  var postTemplate = _.template($('#post-template').html());
 
   // append existing todos (from seed data) to `$toDoList`
   // `_.each` is an "iterator" function provided by Underscore.js
-  _.each(ToDo.all, function (todo, index) {
-    todo.render();
+  _.each(Post.all, function (post, index) {
+    post.render();
   });
 
   // submit form to create new todo
-  $newToDo.on('submit', function(event) {
+  $newPost.on('submit', function(event) {
     event.preventDefault();
 
     // create new toDo object from form data
-    var toDoName = $('#todo-name').val();
-    var toDoDesc = $('#todo-desc').val();
-    var toDo = new ToDo(toDoName, toDoDesc);
+    var postDate = $('#post-date').val();
+    var postText = $('#post-text').val();
+    var post= new Post(postDate, postText);
 
     // save toDo
-    toDo.save();
+    post.save();
 
     // render toDo
-    toDo.render();
+    post.render();
 
     // reset the form
-    $newToDo[0].reset();
-    $('#todo-name').focus();
+    $newPost[0].reset();
+    $('#post-due').focus();
   });
 
   // add class to todo on click to mark it as done
-  $toDoList.on('click', '.todo-text', function() {
+  $postList.on('click', '.post-text', function() {
     $(this).toggleClass('done');
   });
 
   // remove todo from model and view
-  $toDoList.on("click", ".delete", function () {
-    var $todo = $(this).closest(".todo");
-    var index = $todo.attr('data-index');
+  $postList.on("click", ".delete", function () {
+    var $post = $(this).closest(".post");
+    var index = $post.attr('data-index');
 
     // remove todo from the `ToDo.all` array (model)
-    ToDo.all.splice(index, 1);
-    console.log(ToDo.all);
+    post.all.splice(index, 1);
+    console.log(Post.all);
 
     // remove todo from the DOM (view)
-    $todo.remove();
+    $post.remove();
 
     // reset indexes in DOM to match `ToDo.all` array
     // $.each loops through DOM elements
-    $('.todo').each(function(index) {
+    $('.post').each(function(index) {
       $(this).attr('data-index', index);
     });
   });
